843d3a00d2103fa1c2ca46a829022c9f934ba06d
Antonio Guillén <antonioguillengarcia123@gmail.com>
2023-05-22 02:04:34 +0200

    Add pagination function :))

--- CineScrape/CineScrapeProject/Pages/MovieDetails.razor.cs
+++ CineScrape/CineScrapeProject/Pages/MovieDetails.razor.cs
@@ -6,11 +6,15 @@ namespace CineScrapeProject.Pages
 {
 	public partial class MovieDetails
 	{
-		private List<Movie> _movies = new List<Movie>();
-		private Movie _movie = new Movie();
-		private List<Review> _reviews = new List<Review>();
+		private const int LIMIT = 10;
+
+		private List<Movie> _movies = new();
+		private Movie _movie = new();
+		private List<Review> _reviews = new();
 		private string _searchName;
 		private string _searchMessage;
+		private List<List<Review>> _paginationReviews = new();
+		private int _page = 1;
 
 		[Parameter]
 		public string MovieId { get; set; }
@@ -21,14 +25,41 @@ namespace CineScrapeProject.Pages
 		public string SearchName { get => _searchName; set { _searchName = value; GetReviews(value, "name"); } }
 		public string SearchMessage { get => _searchMessage; set { _searchMessage = value; GetReviews(value, "message"); } }
 
+		public List<List<Review>> PaginationReviews { get => _paginationReviews; set => _paginationReviews = value; }
+		public int Page { get => _page; set => _page = value > 0 && value <= PaginationReviews.Count ? value : 1; }
+		public int MaxPage { get; set; }
+
 		protected override async Task OnInitializedAsync() => Movies = await Http.GetFromJsonAsync<List<Movie>>("sample-data/movies.json");
 		protected override async Task OnParametersSetAsync()
 		{
 			Movie = GetMovie(Movies, MovieId);
 
 			Reviews = Movie.Reviews;
+
+			MakePagination(Reviews);
+
+			MaxPage = PaginationReviews.Count;
 		}
 
+		private void MakePagination(List<Review> allReviews)
+		{
+			List<Review> reviews = new List<Review>();
+
+			int limitPosition = LIMIT - 1;
+			int totalCount = allReviews.Count;
+
+			for (int i = 0; i < totalCount; i++)
+			{
+				reviews.Add(allReviews[i]);
+
+				if (i == limitPosition || i == totalCount - 1)
+				{
+					PaginationReviews.Add(reviews);
+					limitPosition += LIMIT;
+					reviews = new();
+				}
+			}
+		}
 		private Movie GetMovie(List<Movie> movies, string movieId)
 		{
 			Movie movie = new Movie();
@@ -49,9 +80,9 @@ namespace CineScrapeProject.Pages
 		{
 			List<Review> reviews = new();
 
-			bool name = option == "name";
+			Page = 1;
 
-			if (name)
+			if (option == "name")
 			{
 				SearchMessage = "";
 				Movie.Reviews.ForEach(review =>
@@ -76,6 +107,22 @@ namespace CineScrapeProject.Pages
 
 			return reviews;
 		}
-		private void GetReviews(string value, string option) => Reviews = value != "" ? CheckOption(value, option) : Movie.Reviews;
+		private void GetReviews(string value, string option)
+		{
+			if (value != "")
+			{
+				List<Review> reviews = CheckOption(value, option);
+
+				Reviews = reviews;
+				MaxPage = (int)Math.Ceiling((double)reviews.Count / 10);
+			}
+			else
+			{
+				Reviews = Movie.Reviews;
+				MaxPage = (int)Math.Ceiling((double)Movie.Reviews.Count / 10);
+			}
+		}
+
+		private List<Review> Pagination(List<Review> allReviews) => allReviews.Count > LIMIT ? PaginationReviews[Page - 1] : allReviews;
 	}
 }
